22e9449d203da1c87ad9a1692b6dab7097a7d2b1
nipunadulara <flangodev@gmail.com>
2023-05-18 13:52:49 +0530

    lab 10

---
+++ lab10/graph.cpp
@@ -0,0 +1,70 @@
+#include <list>
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    // A node will 2 entities
+    // 1. data type int called label
+    // 2. a int type list called neighbours
+    int label;
+    list<int> neighbours;
+};
+
+struct Graph
+{
+    // graph will have an array of type "node" with length specified by n
+    int n = 8;
+    Node *nodes = new Node[n];
+
+    void intializenodes()
+    {
+        // iterate through the nodes and assign labels
+        for (int i = 0; i < n; i++)
+        {
+            nodes[i].label = i + 1;
+        }
+    }
+
+    void addedge(int u, int v)
+    {
+        nodes[u - 1].neighbours.push_back(v);
+
+        nodes[v - 1].neighbours.push_back(u);
+    }
+
+    void print()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << "|" << nodes[i].label << "-->";
+            for (auto x : nodes[i].neighbours)
+            {
+                cout << "->" << x;
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    Graph *g = new Graph;
+    g->intializenodes();
+    // add edges for the graphs here.
+    g->addedge(1, 2);
+    g->addedge(1, 3);
+    g->addedge(1, 5);
+    g->addedge(1, 4);
+    g->addedge(2, 6);
+    g->addedge(2, 3);
+    g->addedge(4, 6);
+    g->addedge(4, 8);
+    g->addedge(5, 6);
+    g->addedge(5, 8);
+    g->addedge(4, 7);
+    g->addedge(5, 7);
+
+    // print the graph adjaceny list
+    g->print();
+}
